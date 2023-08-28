fd5eb93d82b60f997f7a8b76bc4699a15c48c2c5
Arjun Girish <128234597+arjungirish01@users.noreply.github.com>
2023-08-29 00:10:45 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,10 +5,12 @@ import "./App.css";
 import Confetti from "react-confetti";
 
 function App() {
-    // console.log(nanoid());
+
     const [dice,setDice]=React.useState(getDice());
     const [tenzies,setTenzies]=React.useState(false);
-
+    const [moves,setMoves]=React.useState(0);
+    const [best,setBest]=React.useState();
+   
     function getDice(){
         let myArray=[];
         for(let i=0;i<10;i++){
@@ -29,12 +31,24 @@ function App() {
          />)
 
          React.useEffect(()=>{
+            localStorage.setItem("best",JSON.stringify(Number.MAX_SAFE_INTEGER));
+         },[])
+
+         React.useEffect(()=>{
             const firstValue=dice[0].value;
             const allValues=dice.every(dice=>dice.value===firstValue);
             const allHeld=dice.every(dice=>dice.isHeld);
             if(allValues&&allHeld){
                 setTenzies(true);
                 console.log("You Won");
+                const prevBest=JSON.parse(localStorage.getItem("best"))||Number.MAX_SAFE_INTEGER;
+                console.log(prevBest)
+                if(prevBest>moves){
+                    localStorage.setItem("best",JSON.stringify(moves));
+                    setBest(moves);
+                    console.log(JSON.parse(localStorage.getItem("best")))
+                }
+
             }
          },[dice])
     // function generateDice(){
@@ -46,6 +60,7 @@ function App() {
     // }
 
     function rollDice(){
+        setMoves(prevMove=>prevMove+1);
         setDice(prevDice=>prevDice.map(value=>{
             return value.isHeld?value
             :{value:Math.ceil(Math.random()*6),isHeld:false,id:nanoid()}
@@ -58,24 +73,33 @@ function App() {
             })
         )  
     }
-    console.log(dice);
 
     function resetGame(){
         setDice(getDice());
         setTenzies(false);
+        setMoves(0);
     }
 
   return (
     <main className="App">
+        {tenzies?<Confetti />:(<div></div>)}
+        
         <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        
         <div className="dice">
             {diceElements} 
         </div>
+        
         {tenzies?(<div className="won">Congratulations!!</div>):(<div></div>)}
+        
         {!tenzies?(<button className="rollDice" onClick={rollDice}>Roll Dice</button>)
         :(<button className="resetGame" onClick={resetGame}>Reset Game</button>)}
-        {tenzies?<Confetti />:(<div></div>)}
+        
+        <div className="results">
+            <div className="moves">Total Moves: {moves}</div>
+            <div className="best">Best: {best}</div>
+        </div>
     </main>
   );
 }
